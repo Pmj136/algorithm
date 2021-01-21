330d688466cb2e61fb10fabba0b0505db4702209
彭明久 <1367559786@qq.com>
2021-01-22 00:11:14 +0800

    冒泡排序

--- src/com/pmj/sort/Bubble.java
+++ src/com/pmj/sort/Bubble.java
@@ -7,13 +7,14 @@ package com.pmj.sort;
  * @since 2021-01-12
  */
 public class Bubble {
-    public static void sort(int[] arr) {
-        for (int i = arr.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
+    //3,2,1
+    public static void sort(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int max = arr[j];
+                    int temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = max;
+                    arr[j + 1] = temp;
                 }
             }
         }
